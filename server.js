65b5ad4dd554c13df00e2a5137a86be33e93dc99
rai1108 <rai1108@naver.com>
2020-09-10 17:31:18 +0900

    Create Customer Database Table & Node.js Database Connection Setting

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,25 +7,26 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ etended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+})
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id': 1,
-        'image': 'https://placeimg.com/64/64/any',
-        'name': '홍길동',
-        'birthday': '961222',
-        'gender': '남자',
-        'job': '대학생'
-        },
-        {
-          'id': 2,
-          'image': 'https://placeimg.com/64/64/any2',
-          'name': '김경훈',
-          'birthday': '901118',
-          'gender': '남자',
-          'job': '회사원'
-          }
-      ]);
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
